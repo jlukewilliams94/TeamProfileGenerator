0c74d8ecbbad0d0650942847059acbbcc6aae1c0
luke williams <jluke94williams@gmail.com>
2020-03-30 15:40:30 -0400

    Updated app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,7 @@
-//const Manager = require("./lib/Manager");
-//const Engineer = require("./lib/Engineer");
-//const Intern = require("./lib/Intern");
-const inquirer = require("Inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("./node_modules/inquirer");
 const path = require("path");
 const fs = require("fs");
 ​
@@ -9,7 +9,6 @@ const fs = require("fs");
 //const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 //const render = require("./lib/htmlRenderer");
-
 let employeeArray = [];
 
 const prompts = [{
@@ -39,14 +38,60 @@ const prompts = [{
 }
 ];
 
-inquirer
-    .prompt(prompts
-    ).then((responseA) => {
-        console.log(responseA)
+async function prompt(){
+    let finishResponse = '';
+    do{
+        try{
+            responseA = await inquirer.prompt(prompts)
+            let responseB = '';
 
-})
+            if(responseA.role === "Manager"){
+                responseB = await inquirer.prompt([{
+                    name: "officeNumber",
+                    type: "input",
+                    message: "What is the Manager's office number?"
+                }])
+                const manager = new Manager(responseA.name, responseA.id, responseA.email, responseA.role, responseB.officeNumber)
+                employeeArray.push(manager)
+            } else if (responseA.role === "Engineer") {
+                responseB = await inquirer.prompt([{
+                    name: "github",
+                    type: "input",
+                    message: "What is the Engineer's github username?"
+                }])
+                const engineer = new Engineer(responseA.name, responseA.id, responseA.email, responseA.role, responseB.github)
+                employeeArray.push(engineer)
+            } else if (responseA.role === "Intern") {
+                responseB = await inquirer.prompt([{
+                    name: "school",
+                    type: "input",
+                    message: "What college does the Intern go to?"
+                }])
+                const intern = new Intern(responseA.name, responseA.id, responseA.email, responseA.role, responseB.school)
+                employeeArray.push(intern)
+            }
 
-​
+        }catch(err){
+            return console.log(err)
+        }
+        console.log(employeeArray)
+
+        finishResponse = await inquirer.prompt([{
+            name: "finish",
+            type: "list",
+            message: "Do you want to add another Employee?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }])
+    } while (finishResponse === "Yes")
+
+​}
+
+
+
+
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
